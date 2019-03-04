1796cba6b9eb8932c90aa06d489096392e5c383f
Paige Gorry <paigeegorry@gmail.com>
2019-03-04 13:45:28 -0800

    first thousand words in the browser

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import TopWords from '../words/TopWords';
 
 export default function App() {
-  return <h1>Hi</h1>
+  return (
+    <>
+      <h1>Hello</h1>
+      <TopWords />
+    </>
+  );
 }
---
+++ src/components/words/Words.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Words({ words }) {
+  const wordsList = words.map(word => {
+    return (
+      <li key={word}>{word}</li>
+    );
+  });
+  return (
+    <ul>
+      {wordsList}
+    </ul>
+  );
+}
+
+Words.propTypes = {
+  words: PropTypes.array.isRequired
+};
+
+export default Words;
