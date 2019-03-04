c96a55acac684d28a9eabae088fd329dc2281d77
Marcy Silverman <marcysilve225@gmail.com>
2019-03-04 13:56:56 -0800

    router path works

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import TopWords from '../../containers/words/TopWords';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 export default function App() {
   return (
-    <TopWords />
+    <>
+    <Router> 
+      <Route path="/topwords" exact component={TopWords} />
+    </Router>
+    </>
   );
 }
