f37a21f2dd4bc083f9a48810c99e17b4d61f77e0
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-04 13:56:02 -0800

    can return word count based on routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import TopWords from '../../containers/words/TopWords';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 export default function App() {
   return (
-    < >
-      <h1>Hi</h1>
-      <TopWords />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={'/'} component={TopWords} />
+        <Route exact path={'/words/:count'} component={TopWords} />
+        <Redirect to={'/'} />
+      </Switch>
+    </Router>
   );
 }
--- src/containers/words/TopWords.js
+++ src/containers/words/TopWords.js
@@ -3,8 +3,8 @@ import { getFirstWords } from '../../selectors/words';
 import Words from '../../components/words/Words';
 
 // TopWords should retrieve the first 1000 words
-const mapStateToProps = state => ({
-  words: getFirstWords(state, 1000)
+const mapStateToProps = (state, props) => ({
+  words: getFirstWords(state, props.match.params.count || 1000)
 });
 
 // const mapDispatchToProps = dispatch => ({
