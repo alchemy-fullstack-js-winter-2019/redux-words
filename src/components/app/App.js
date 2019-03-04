42ae79ae441da4728512254d40009affe84ab0ba
Shaba9 <shaba92@gmail.com>
2019-03-04 13:55:41 -0800

    / route added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import TopWords from '../../containers/words/TopWords';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 export default function App() {
-  return <TopWords />;
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route path="/" component={TopWords} />
+        </Switch>
+      </>
+    </Router>
+  );
 }
