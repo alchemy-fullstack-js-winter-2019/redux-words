4bd1a8a6f771b6e1c4cb449beacbc27036312a4d
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-04 13:55:46 -0800

    can query in the URl by number to return a list relative the to URL

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FirstWords from '../../containers/words/TopWords';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-export default function App() {
-    return (
-      <>
-        <h1>Hi</h1>
-        <FirstWords/>
-      </>
-    );
+
+export default class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={FirstWords}/>
+                    <Route exact path="/words/:count" component={FirstWords}/>
+                </Switch>
+            </Router>
+        );
+    }
 }
--- src/containers/words/TopWords.js
+++ src/containers/words/TopWords.js
@@ -2,8 +2,10 @@ import { Words } from '../../components/words/Words';
 import { getFirstWords } from '../../selectors/words';
 import { connect } from 'react-redux';
 
-const mapStateToProps = state => ({
-    words: getFirstWords(state, 1000)
+
+
+const mapStateToProps = (state, props) => ({
+    words: getFirstWords(state, props.match.params.count || 1000)
 });
 
 export default connect(mapStateToProps)(Words);
