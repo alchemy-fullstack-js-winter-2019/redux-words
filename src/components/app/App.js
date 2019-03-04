10390b3d28d22ed2f7fed040c99c95b193a8deb9
Paige Gorry <paigeegorry@gmail.com>
2019-03-04 14:07:00 -0800

    count router working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import TopWords from '../words/TopWords';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <h1>Hello</h1>
-      <TopWords />
+      <Router>
+        <Switch>
+          <Route exact to="/" component={TopWords} />
+          <Route exact to="/words/:count" component={TopWords} />
+        </Switch>
+      </Router>
     </>
   );
 }
