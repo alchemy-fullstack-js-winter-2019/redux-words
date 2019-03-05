6cea04cb4f2ee355c1d7e97a1ad2e5d3b9b1f7f0
cruzayye <johnnyc010392@gmail.com>
2019-03-04 22:44:39 -0800

    got rid of comments

--- src/selectors/words.js
+++ src/selectors/words.js
@@ -1,9 +1,6 @@
 const getSubWords = (arr, count) => arr.slice(0, count);
 export const getWords = state => state.words.dictionary;
 
-// export const getFirstWords = (state, count) => {
-//   return getWords(state).slice(0, count);
-// };
 export const getFirstWords = (state, count) => {
   return getSubWords(getWords(state), count);
 };
@@ -16,6 +13,7 @@ export const getFirstWordsByTerm = (state, count) => {
     return word.includes(searchTerm);
   }), count);
 };
+
 function selectorPerformance(fn) {
   const start = performance.now();
   const results = fn();
