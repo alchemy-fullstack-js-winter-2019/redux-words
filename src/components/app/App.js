51c876b679721918d927642cafc0f6b9a9c10184
Sophie Graham <sophiergraham1@gmail.com>
2019-03-05 11:52:28 -0800

    still working, need to finish later today

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,7 @@ import {
   Route, 
   Switch
 } from 'react-router-dom';
-import TopWords from '../../containers/words/TopWords';
+import { TopWords, getFirstWords } from '../../containers/words/TopWords';
 
 export default function App() {
   return (
@@ -12,6 +12,7 @@ export default function App() {
       <Switch>
         <Route exact path="" component={TopWords} />
         <Route exact path="/words/:count" component={TopWords} />
+        <Route exact path="/" component={getFirstWords} />
       </Switch>
     </Router>
   );
