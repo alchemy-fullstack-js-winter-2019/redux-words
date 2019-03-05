29e234627785892d9bf82f4b0f1f5cd9f5402590
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-05 11:08:38 -0800

    cleanup for submit

--- src/components/words/Searchable.js
+++ src/components/words/Searchable.js
@@ -2,14 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchWords from '../../containers/words/SearchWords';
 import TopWords from '../../containers/words/TopWords';
-import queryString from 'query-string';
 
-const params = queryString.parse(location.search);
-// eslint-disable-next-line
-console.log(params.searchTerm); // this is the searchTerm in pathname
-
-// eslint-disable-next-line
-const Searchable = ({ location, match }) => {
+const Searchable = ({ match }) => {
   return (
     < >
       <SearchWords />
@@ -19,8 +13,7 @@ const Searchable = ({ location, match }) => {
 };
 
 Searchable.propTypes = {
-  match: PropTypes.object.isRequired,
-  location: PropTypes.object
+  match: PropTypes.object.isRequired
 };
 
 export default Searchable;
