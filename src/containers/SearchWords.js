ddda7efc9f4841738e8f9b7387ceec053ea54a76
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-05 13:19:39 -0800

    added travis and scripts

--- src/containers/SearchWords.js
+++ src/containers/SearchWords.js
@@ -1,19 +1,19 @@
 import { connect } from 'react-redux';
 import { getTopWordsByTerm } from '../selectors/words';
-import {updateSearchTerm } from '../components/actions/words'; 
+import { updateSearchTerm } from '../components/actions/words';
 import SearchForm from '../components/words/SearchForm';
 
 const mapPropsToState = state => ({
-    searchTerm: getTopWordsByTerm(state)
+  searchTerm: getTopWordsByTerm(state)
 }
-)
+);
 const mapDispatchToState = dispatch => ({
-    onChange({ target }) {
-        dispatch(updateSearchTerm(target.value));
-    }
-})
+  onChange({ target }) {
+    dispatch(updateSearchTerm(target.value));
+  }
+});
 
 export default connect(
-    mapPropsToState,
-    mapDispatchToState
+  mapPropsToState,
+  mapDispatchToState
 )(SearchForm);
