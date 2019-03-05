3de2b27afc3ffa7cce1deed1f542ad1269f355a3
Paige Gorry <paigeegorry@gmail.com>
2019-03-05 09:13:12 -0800

    search query is working

--- src/containers/words/SearchWords.js
+++ src/containers/words/SearchWords.js
@@ -1,10 +1,34 @@
 import { connect } from 'react-redux';
+import React, { PureComponent } from 'react';
 import SearchForm from '../../components/words/SearchForm';
 import { updateSearchTerm } from '../../actions/words';
 import { getSearchTerm } from '../../selectors/words';
+import PropTypes from 'prop-types';
+import store from '../../store';
 
+class Searchable extends PureComponent {
+  static propTypes = {
+    term: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    store.dispatch(updateSearchTerm(this.props.term));
+  }
+
+  render(){
+    return (
+      <SearchForm
+        term={this.props.term}
+        onChange={this.props.onChange}
+      />
+    );
+  }
+}
+
+const searchQuery = location.search.split('=').slice(1).join('');
 const mapStateToProps = state => ({
-  term: getSearchTerm(state)
+  term: searchQuery || getSearchTerm(state)
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -16,4 +40,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SearchForm);
+)(Searchable);
