68a47c8dfb115e4cf88181154ac988227e55f8a8
Shaba9 <shaba92@gmail.com>
2019-03-05 09:34:28 -0800

    passed match as prop to TopWords

--- src/components/words/Searchable.js
+++ src/components/words/Searchable.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchWords from '../../containers/words/SearchWords';
 import TopWords from '../../containers/words/TopWords';
 
-export default function Searchable() {
+function Searchable({ match }) {
   return (
     <>
       <SearchWords />
+      <TopWords match={match} />
     </>
   );
 }
+
+Searchable.propTypes = {
+  match: PropTypes.object.isRequired
+};
+
+export default Searchable;
