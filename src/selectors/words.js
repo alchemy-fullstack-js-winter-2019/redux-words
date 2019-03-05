26da2678e287619e0b84b3083c4972b590a6be97
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-05 09:05:19 -0800

    was on the route /works/3 instead of words/3... thought it was broken

--- src/selectors/words.js
+++ src/selectors/words.js
@@ -1,3 +1,20 @@
+const getSubWords = (arr, count) => arr.slice(0, count);
+
+export const getWords = state => state.words.dictionary;
+
+export const getFirstWords = (state, count) => {
+  return getSubWords(getWords(state), count);
+};
+
+export const getSearchTerm = state => state.words.searchTerm;
+
+export const getFirstWordsByTerm = (state, count) => {
+  const searchTerm = getSearchTerm(state);
+  return getSubWords(getWords(state).filter(word => {
+    return word.includes(searchTerm);
+  }), count);
+};
+
 function selectorPerformance(fn) {
   const start = performance.now();
   const results = fn();
