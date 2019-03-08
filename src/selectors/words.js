e834da01acdb7f3187607c7764f4808f36b63ac1
Dyanna Tolman <dyannast@gmail.com>
2019-03-08 15:00:07 -0800

    fixed search term and now displaying and performing correctly

--- src/selectors/words.js
+++ src/selectors/words.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+// import { createSelector } from 'reselect';
 
 const getSubWords = (arr, count) => arr.slice(0, count);
 
@@ -9,14 +9,13 @@ export const getFirstWords = (state, count) => {
 };
 export const getSearchTerm = state => state.words.searchTerm;
 
-
-export const getFirstWordsByTerm = createSelector(
-  getWords, 
-  getSearchTerm,
-  //below arguments comes from container TopWords
-  (_, count) => count,
-  badSelector
-);
+export const getFirstWordsByTerm = (state, count) => {
+  const searchTerm = getSearchTerm(state);
+  // return getSubWords(selectorPerformance(badSelector.bind(null, getWords(state), searchTerm)), count);
+  return getSubWords(getWords(state).filter(word => {
+    return word.includes(searchTerm);
+  }), count);
+};
 
 export const getColor = state => state.words.color;
   
@@ -28,19 +27,4 @@ function selectorPerformance(fn) {
   return results;
 }
   
-function badSelector(words, searchTerm, count) {
-  const filtered = [];
-  for(let j = 0; j < words.length; j++) {
-    if(!filtered.some(w => w === words[j]) && words[j].includes(searchTerm)) {
-      filtered.push(words[j]);
-    }
-  }
-  return getSubWords(filtered, count);
-}
-  
-// export const getFirstWordsByTerm = (state, count) => {
-//   const searchTerm = getSearchTerm(state);
-//   return getSubWords(getWords(state).filter(word => {
-//     return word.includes(searchTerm);
-//   }), count);
-// };
+
