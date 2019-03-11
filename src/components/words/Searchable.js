8371f1f69ef684b1b24aecec67f71a9e2e0b8be9
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-11 09:05:40 -0700

    travis typos

--- src/components/words/Searchable.js
+++ src/components/words/Searchable.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import SearchWords from '../../containers/words/SearchWords';
 import TopWords from '../../containers/words/TopWords';
-import PropTypes from 'prop-types';
-
 
 function SearchAble() {
     return (
