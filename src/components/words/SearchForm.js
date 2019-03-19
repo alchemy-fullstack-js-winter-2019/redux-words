fded2a6fdb6fe55f4a84c50fe4c22af92c9f35f9
Sophie Graham <sophiergraham1@gmail.com>
2019-03-19 11:51:41 -0700

    search form component

---
+++ src/components/words/SearchForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SearchTerm({ searchTerm, onChange }) {
+  return (
+    <input type="text" onChange={onChange} value={searchTerm} />
+  );
+}
+
+SearchTerm.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default SearchTerm;
